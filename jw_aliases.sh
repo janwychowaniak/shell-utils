6e80427be4dc0137d9d24825a72fe0b807856c4d
Jan Wychowaniak <darkhleb@poczta.fm>
2018-10-03 21:58:56 +0200

    initial ci

---
+++ jw_aliases.sh
@@ -0,0 +1,13 @@
+export EDITOR=vim
+
+
+alias diff='colordiff'
+alias jwdate='date +%Y-%m-%d'
+alias jwdate_l='echo -n "Dzis:  " && date +%Y-%m-%d && echo && ncal -M && echo'
+alias grep='grep --color=auto'
+alias jwshred='shred --iterations=0 --zero --remove --verbose'
+alias jwmount='mount | grep ^\/dev'
+
+alias jwytgrep='grep -o [^\"]*\/\watch\?v\=[^\&\"]*'
+
+

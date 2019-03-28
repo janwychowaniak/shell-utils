6e19bcd46a9f7c2390d2730e6ca1482373bf80bf
Jan Wychowaniak <43786923+janwychowaniak@users.noreply.github.com>
2019-03-28 16:29:40 +0100

    bits of additional description

--- jw_aliases.sh
+++ jw_aliases.sh
@@ -1,13 +1,14 @@
-export EDITOR=vim
+# A small collection of miscellaneous useful aliases
 
+
+export EDITOR=vim
 
 alias diff='colordiff'
 alias jwdate='date +%Y-%m-%d'
-alias jwdate_l='echo -n "Dzis:  " && date +%Y-%m-%d && echo && ncal -M && echo'
+alias jwdate_l='echo -n "Today:  " && date +%Y-%m-%d && echo && ncal -M && echo'
 alias grep='grep --color=auto'
 alias jwshred='shred --iterations=0 --zero --remove --verbose'
 alias jwmount='mount | grep ^\/dev'
 
 alias jwytgrep='grep -o [^\"]*\/\watch\?v\=[^\&\"]*'
-
 

c78dbf5cd380e398532ef4128ab3f07e13891871
Jan Wychowaniak <43786923+janwychowaniak@users.noreply.github.com>
2019-03-28 16:22:26 +0100

    some of the code bits better documented

--- jw_functions__misc.sh
+++ jw_functions__misc.sh
@@ -1,6 +1,11 @@
+# A collection of miscellaneous functions that are hard to categorize otherwise
+
+
 jwgoogle()
 {
 cat 1>&2 <<'EOF'
+
+=================== GOOGLE =================== (notes regarding things I keep forgetting in terms of using the Gmail inbox filters)
 
  older_than:1d
  newer_than:1d
@@ -16,59 +21,62 @@ EOF
 }
 
 
-jwtvnames() {
-  DATEHASH=`date +%Y%m%d`
-  SEED=$(( ( RANDOM % 100000 ) + 10000 ))
-  NUMER="$DATEHASH$SEED"
-  echo "skocznie-$NUMER"
-  echo "schodki-$NUMER"
-  echo "kanaly-$NUMER"
-  echo "hidden-$NUMER"
-  echo "inne-$NUMER"
-  echo "kilka-$NUMER"
-  echo "zagranie-$NUMER"
-}
-
-jwtvcrop() {
-  case $# in
-  "0")
-    echo " *** " $FUNCNAME "arg[s]"
-  ;;
-  "1")
-    local PLIK_IN=$1
-    local PLIK_TEMP="$$-"$PLIK_IN
-    convert $PLIK_IN -crop 1812x823+56+165 $PLIK_TEMP
-    mv $PLIK_TEMP $PLIK_IN
-  ;;
-  *)
-    for p in $@
-    do
-      echo $p
-      local PLIK_IN=$p
-      local PLIK_TEMP="$$-"$PLIK_IN
-      convert $PLIK_IN -crop 1812x823+56+165 $PLIK_TEMP
-      mv $PLIK_TEMP $PLIK_IN
-    done
-  ;;
-  esac
-}
-
-
 jwrandnumgen()
 {
     if [ $# -ne 1 ]; then
         echo
-        echo "${FUNCNAME[0]} LICZBA"
+        echo "$FUNCNAME  LIMIT"
         echo
-        echo "    Skrypt generuje losowa liczbe z zakresu [1, LICZBA]."
+        echo "    A small convenience function for generating a pseudo-random integer number from the range of [1, LIMIT]."
         echo
         return 1
     fi
     
     python3 -c \
     "import sys;
-LICZBA=sys.argv[1];
+LIMIT=sys.argv[1];
 from random import randint;
-print(randint(0, int(LICZBA)))" $1
+print(randint(0, int(LIMIT)))" $1
+}
+
+
+
+jwtvnames()
+{
+    DATEHASH=`date +%Y%m%d`
+    SEED=$(( ( RANDOM % 100000 ) + 10000 ))
+    NUMER="$DATEHASH$SEED"
+    echo "skocznie-$NUMER"
+    echo "schodki-$NUMER"
+    echo "kanaly-$NUMER"
+    echo "hidden-$NUMER"
+    echo "inne-$NUMER"
+    echo "kilka-$NUMER"
+    echo "zagranie-$NUMER"
+}
+
+jwtvcrop()
+{
+    case $# in
+    "0")
+        echo " *** " $FUNCNAME "arg[s]"
+    ;;
+    "1")
+        local PLIK_IN=$1
+        local PLIK_TEMP="$$-"$PLIK_IN
+        convert $PLIK_IN -crop 1812x823+56+165 $PLIK_TEMP
+        mv $PLIK_TEMP $PLIK_IN
+    ;;
+    *)
+        for p in $@
+        do
+            echo $p
+            local PLIK_IN=$p
+            local PLIK_TEMP="$$-"$PLIK_IN
+            convert $PLIK_IN -crop 1812x823+56+165 $PLIK_TEMP
+            mv $PLIK_TEMP $PLIK_IN
+        done
+    ;;
+    esac
 }
 

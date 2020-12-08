9b3ec9cbed58db65208558572799ce9b7c6ba8ab
Jan Wychowaniak <43786923+janwychowaniak@users.noreply.github.com>
2020-12-08 11:55:33 +0100

    _l -> l

--- jw_aliases.sh
+++ jw_aliases.sh
@@ -6,15 +6,15 @@ export EDITOR=vim
 alias diff='colordiff'
 alias jwwdiff="wdiff -n -w $'\033[30;41m' -x $'\033[0m'  -y $'\033[30;42m' -z $'\033[0m'"
 alias jwdate='date +%Y-%m-%d'
-alias jwdate_l='echo && echo -n "Today:  " && date +%Y-%m-%d && echo && ncal -M && echo'
+alias jwdatel='echo && echo -n "Today:  " && date +%Y-%m-%d && echo && ncal -M && echo'
 alias grep='grep --color=auto'
 alias jwshred='shred --iterations=0 --zero --remove --verbose'
 alias jwmount="mount | grep ^\/dev | awk '{print \$1 \" -> \" \$3}'"
-alias jwmount_l='mount | grep ^\/dev'
+alias jwmountl='mount | grep ^\/dev'
 
 alias jwytgrep='grep -o [^\"]*\/\watch\?v\=[^\&\"]*'
 
 alias jwsl="screen -ls | grep \"^[[:space:]]\" | tr \".\" \" \" | awk '{print \"   \" \$2}' | sort"
-alias jwsl_l='screen -ls'
+alias jwsll='screen -ls'
 alias jwsr='screen -r'
 alias jwss='screen -S'

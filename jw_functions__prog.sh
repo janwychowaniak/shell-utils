9d474a7a2e6836e891c5d848434736a702a5729d
Jan Wychowaniak <43786923+janwychowaniak@users.noreply.github.com>
2023-10-09 11:03:24 +0200

    parametrized Python virtual environment management

--- jw_functions__prog.sh
+++ jw_functions__prog.sh
@@ -2,8 +2,6 @@
 # Some are just notes on how to use certain tools.
 
 
-alias jwvec__create-virtualenv="python3 -m venv venv"
-alias jwvea__activate-virtualenv="source venv/bin/activate"
 alias jwpyl__pylint3-dRC="pylint3 -d R,C"
 
 # --------------------------------------------------------
@@ -39,6 +37,74 @@ jwcoflo()
 #
 # python ####################################################################################################################################
 #
+jwvec__create-virtualenv() {
+  # Create and configure a Python virtual environment with optional package installation.
+  #
+  # Usage: jwvec__create-virtualenv [ENV_NAME] [REQ_FILES...]
+  #
+  # This function creates a Python virtual environment in the current directory and
+  # optionally installs packages from specified requirements files.
+  #
+  # Arguments:
+  #   ENV_NAME (optional): Name for the virtual environment (default: 'venv').
+  #   REQ_FILES (optional): One or more requirement files to install packages from.
+  #
+  # If no environment name is provided, the default name 'venv' is used. If no
+  # requirement files are specified, it defaults to 'requirements.txt'. The function
+  # checks for the existence of the specified environment name or requirements files
+  # before creating or installing, ensuring there are no naming conflicts.
+  #
+  # Example usage:
+  #   jwvec__create-virtualenv                              # Create 'venv' and install packages from 'requirements.txt'
+  #   jwvec__create-virtualenv myenv                        # Create 'myenv' and install packages from 'requirements.txt'
+  #   jwvec__create-virtualenv customenv req1.txt req2.txt  # Create 'customenv' and install packages from 'req1.txt' and 'req2.txt'
+  #
+  # Note: This function assumes 'python3' and 'pip' are available on the system.
+  #
+  # Returns:
+  #   0: Success
+  #   1: Error (e.g., environment name or requirements file conflict)
+
+  local venv_name="${1:-venv}"  # Use the provided argument or 'venv' as the default
+  local req_files=("${@:2}")    # Get additional arguments as requirement files
+
+  # Check if an object with the given name exists
+  if [ -e "$venv_name" ]; then
+    echo "Error: An object with the name '$venv_name' already exists. Choose a different environment name."
+    return 1  # Return an error code
+  fi
+
+  # Create the virtual environment
+  echo "Creating virtual environment: $venv_name"
+  python3 -m venv "$venv_name"
+
+  # Check if any requirements files were provided
+  if [ ${#req_files[@]} -eq 0 ]; then
+    req_files=("requirements.txt")  # Use 'requirements.txt' as default if none provided
+  fi
+
+  # Activate the virtual environment
+  source "$venv_name/bin/activate"
+
+  # Install packages from specified requirements files
+  for req_file in "${req_files[@]}"; do
+    if [ -f "$req_file" ]; then
+      echo
+      echo "From $req_file:"
+      python3 -m pip install -r "$req_file"
+    else
+      echo "Warning: Requirements file '$req_file' not found (in case any is expected)."
+    fi
+  done
+}
+
+jwvea__activate-virtualenv() {
+  local venv_name="${1:-venv}"  # Use the provided argument or 'venv' as the default
+  source "$venv_name/bin/activate"
+}
+
+# ---
+
 jwpyc__pycallgraph()
 {
     FILEPARAM=${1:-SCRIPT.py}
